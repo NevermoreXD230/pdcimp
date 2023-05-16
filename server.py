262ba815f6dc0cf44bff61d3be64775ed7b60037
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 13:05:35 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,73 +1,50 @@
 import random
-import time
-from multiprocessing import Process, Queue
+from multiprocessing import Process
 
 def fermat(n):
     if n == 1:
         return True
 
-    for _ in range(10):  # Perform the test multiple times
+    for _ in range(10):
         r = random.randint(1, n-1)
         if pow(r, n-1, n) != 1:
             return False
 
     return True
 
-def server(num_workers):
-    primes_found = []
-    num_queue = Queue()
-    result_queue = Queue()
-    assigned_numbers = Queue()  # Queue for assigning numbers to workers
+def generate_primes():
+    
+    for _ in range(10**7):
+        _ = random.random()  
 
-    def worker_task(worker_id):
-        while True:
-            result_queue.put(worker_id)  # Signal worker availability to the server
+def worker(worker_id, worker_ip):
+    num = random.randint(10**15, 10**16)
+    prime = fermat(num)
 
-            num = assigned_numbers.get()  # Wait for a number to test
-            if num is None:
-                break
+    print(f"Worker {worker_id} ({worker_ip}) received number: {num}")
+    generate_primes()
 
-            if fermat(num):
-                result_queue.put((worker_id, num))  # Report prime number to server
+    if prime:
+        print(f"Worker {worker_id} ({worker_ip}) successfully verified a potential prime number: {num}")
 
-    # Start worker processes
+if __name__ == "__main__":
+    num_workers = 2
+
+    worker_ips = {
+        0: "192.168.83.46",
+        1: "192.168.83.185"
+    }
+
+    print("Starting prime number verification process...")
+    print(f"Number of workers: {num_workers}\n")
+
     workers = []
     for i in range(num_workers):
-        p = Process(target=worker_task, args=(i,))
+        p = Process(target=worker, args=(i, worker_ips[i]))
         p.start()
         workers.append(p)
-
-    next_number = 2
-    while True:
-        for p in primes_found:
-            if next_number % p == 0:
-                break
-        else:
-            num_queue.put(next_number)  # Send number to the queue for testing
-
-        # Assign numbers to available workers
-        while not num_queue.empty():
-            worker_id = result_queue.get()
-            if worker_id is not None:
-                num = num_queue.get()  # Get the number to assign to the worker
-                assigned_numbers.put(num)  # Assign the number to the worker
-
-        while not result_queue.empty():
-            data = result_queue.get()
-            if isinstance(data, tuple):
-                worker_id, prime = data
-                primes_found.append(prime)
-                print(f"Worker {worker_id} found prime: {prime}")
-
-        next_number += 1
-
-    # Stop worker processes
-    for _ in range(num_workers):
-        assigned_numbers.put(None)
 
     for p in workers:
         p.join()
 
-if __name__ == "__main__":
-    num_workers = 2  # Adjust the number of workers as per your requirement
-    server(num_workers)
+    print("\nPrime number verification process completed.")
