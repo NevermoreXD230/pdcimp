fbd67fda9226a2b3814810cf8c2fc83bd7463910
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 06:21:20 +0500

    Update prime_distributed.erl

--- prime_distributed.erl
+++ prime_distributed.erl
@@ -1,65 +1,37 @@
 -module(prime_distributed).
+-export([start/1, stop/0, process/2]).
 
--export([start/2, master/4, worker/1, is_prime/1]).
+start(Workers) ->
+    spawn(fun() -> loop(Workers) end).
 
-start(N, NumOfWorkers) ->
-    spawn(prime_distributed, master, [N, NumOfWorkers, self(), []]).
+stop() ->
+    self() ! stop.
 
-master(N, NumOfWorkers, Parent, PrimeList) ->
-    K = trunc(math:sqrt(N))+1,
-    Lists = lists:foldl(
-        fun(K_, Lists_) ->
-            Start = (K_-1)*K + 1,
-            End = K_*K,
-            [ {Start_, End_} || Start_ <- lists:seq(Start, N, K_), End_ <- lists:seq(Start_, min(End, N)), Start_ =< End_ ] ++ Lists_
-        end,
-        [], lists:seq(1,K)),
-    Workers = [ spawn(prime_distributed, worker, []) || _ <- lists:seq(1,NumOfWorkers)],
-    spawn_link(prime_distributed, collector, [Parent, length(Lists), PrimeList]),
-    lists:foreach(
-        fun(List_) ->
-            {Worker, WorkersRest} = lists:split(1, Workers),
-            Worker ! {self(), List_},
-            Workers = WorkersRest
-        end,
-        Lists),
+process(N, Client) ->
+    self() ! {process, N, Client},
+    receive
+        {result, P} ->
+            Client ! {result, P}
+    end.
+
+loop(Workers) ->
+    receive
+        {process, N, Client} ->
+            spawn(fun() -> handle(N, Client, Workers) end),
+            loop(Workers);
+        stop ->
+            ok
+    end.
+
+handle(N, Client, [Worker | Rest]) ->
+    case rpc:call(Worker, fermat, process, [N]) of
+        {ok, P} ->
+            Client ! {result, P},
+            Worker ! {free, self()},
+            loop([Worker | Rest]);
+        _ ->
+            handle(N, Client, Rest)
+    end;
+handle(_, _, []) ->
     ok.
 
-worker() ->
-    receive
-        {Parent, {Start, End}} ->
-            Parent ! {self(), [ N || N <- lists:seq(Start,End), is_prime(N) =:= true ]}
-    end,
-    worker().
-
-collector(Parent, NumOfWorkers, PrimeList) ->
-    receive
-        {From, List} ->
-            collector(Parent, NumOfWorkers, PrimeList ++ List);
-        {'EXIT', _, _} ->
-            {_, WorkersRest} = lists:split(1, lists:droplast(NumOfWorkers, [spawn(prime_distributed, worker, []) || _ <- lists:seq(1,NumOfWorkers) ])),
-            lists:foreach(
-                fun(Worker_) -> Worker_ ! stop end,
-                WorkersRest),
-            exit(killed)
-    after
-        5000 ->
-            {_, WorkersRest} = lists:split(1, lists:droplast(NumOfWorkers, [spawn(prime_distributed, worker, []) || _ <- lists:seq(1,NumOfWorkers) ])),
-            lists:foreach(
-                fun(Worker_) -> Worker_ ! stop end,
-                WorkersRest),
-            exit(timeout)
-    end,
-    Parent ! {primes, PrimeList},
-    exit(normal).
-
-is_prime(N) when N > 1 ->
-    is_prime(N, 2, trunc(math:sqrt(N))+1).
-
-is_prime(N, K, Max) when K < Max ->
-    case N rem K of
-        0 -> false;
-        _ -> is_prime(N, K+1, Max)
-    end;
-is_prime(_, _, _) ->
-    true.
