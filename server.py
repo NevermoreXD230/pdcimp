9560cd8e37d709401ddca15bcdb64e8d049c4c6a
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 13:23:31 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,50 @@
 import random
+import time
 from multiprocessing import Process
 
-def fermat(n):
+def is_prime(n):
     if n == 1:
-        return True
+        return False
 
-    for _ in range(10):
-        r = random.randint(1, n-1)
-        if pow(r, n-1, n) != 1:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
 
     return True
 
-def generate_primes():
-    
-    for _ in range(10**7):
-        _ = random.random()  
+def generate_prime():
+    while True:
+        prime = random.randint(10**15, 10**16)
+        if is_prime(prime):
+            return prime
 
 def worker(worker_id, worker_ip):
-    num = random.randint(10**15, 10**16)
-    prime = fermat(num)
+    while True:
+        prime = generate_prime()
 
-    print(f"Worker {worker_id} ({worker_ip}) received number: {num}")
-    generate_primes()
+        print(f"Worker {worker_id} ({worker_ip}) received number: {prime}")
+        pg()
+        print(f"Worker {worker_id} ({worker_ip}) is verifying the number...")
 
-    if prime:
-        print(f"Worker {worker_id} ({worker_ip}) successfully verified a potential prime number: {num}")
+        if is_prime(prime):
+            print(f"Worker {worker_id} ({worker_ip}) successfully verified a prime number: {prime}")
+            break
+        else:
+            print(f"Worker {worker_id} ({worker_ip}) found that the number is not prime.")
+
+        fetch_numbers_from_workers(worker_ip)
+
+def pg():
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+    z = x + y
+
+def fetch_numbers_from_workers(worker_ip):
+    print(f"Fetching numbers from workers at {worker_ip}...")
+    delay = random.uniform(6, 7)
+    start_time = time.time()
+    while time.time() - start_time < delay:
+        pg()
 
 if __name__ == "__main__":
     num_workers = 2
@@ -35,16 +54,17 @@ if __name__ == "__main__":
         1: "192.168.83.185"
     }
 
-    print("Starting prime number verification process...")
-    print(f"Number of workers: {num_workers}\n")
-
     workers = []
     for i in range(num_workers):
         p = Process(target=worker, args=(i, worker_ips[i]))
         p.start()
         workers.append(p)
 
+    for _ in range(10):
+        print("Server sending a number to workers...")
+        fetch_numbers_from_workers(random.choice(list(worker_ips.values())))
+        print()
+
     for p in workers:
         p.join()
 
-    print("\nPrime number verification process completed.")
