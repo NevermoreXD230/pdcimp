9814feb04fc1240fd4db47a2cb36704421417eba
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 11:09:24 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,55 +1,76 @@
-import Pyro4
+import socket
+import pickle
+from multiprocessing import Process, Queue
 
-@Pyro4.expose
-class PrimeServer:
-    def __init__(self):
-        self.workers = []
-    
-    def add_worker(self, worker_uri):
-        self.workers.append(worker_uri)
-    
-    def find_primes(self, start, end):
+# Define a function to check if a number is prime
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+# Define a function to handle a connection from a worker
+def handle_connection(conn, queue):
+    while True:
+        task = conn.recv(1024)
+        if not task:
+            break
+        start, end = task
         primes = []
-        for i in range(start, end+1):
-            for j in range(2, int(i**0.5)+1):
-                if (i % j) == 0:
-                    break
-            else:
-                primes.append(i)
-        return primes
+        for n in range(start, end+1):
+            if is_prime(n):
+                primes.append(n)
+        queue.put(primes)
+    conn.close()
 
-    def distribute_workload(self, start, end):
-        workload_size = (end - start) // len(self.workers)
-        start_range = start
-        end_range = start_range + workload_size
+# Define a function to start the server
+def start_server():
+    # Set up the server socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('192.168.83.205', 5000))
+    server_socket.listen()
 
-        jobs = []
-        for worker_uri in self.workers:
-            jobs.append((worker_uri, start_range, end_range))
-            start_range = end_range + 1
-            end_range = start_range + workload_size
+    # Create a queue to store the results from the workers
+    results_queue = Queue()
 
-        # Handle remainder workload
-        if end_range - 1 < end:
-            jobs[-1] = (jobs[-1][0], jobs[-1][1], end)
+    # Start two worker processes
+    workers = []
+    for i in range(2):
+        p = Process(target=start_worker, args=(results_queue,))
+        p.start()
+        workers.append(p)
 
-        results = []
-        for worker_uri, start, end in jobs:
-            worker = Pyro4.Proxy(worker_uri)
-            result = worker.find_primes(start, end)
-            results.extend(result)
-        
-        return results
+    # Listen for connections from the workers
+    while True:
+        conn, addr = server_socket.accept()
+        p = Process(target=handle_connection, args=(conn, results_queue))
+        p.start()
 
-def main():
-    server = PrimeServer()
-    daemon = Pyro4.Daemon(host="192.168.83.205")
-    uri = daemon.register(server)
-    ns = Pyro4.locateNS(host="192.168.83.205")
-    ns.register("prime.server", uri)
+    # Wait for the worker processes to finish
+    for p in workers:
+        p.join()
 
-    print("Prime server ready.")
-    daemon.requestLoop()
+# Define a function to start a worker
+def start_worker(queue):
+    # Set up the worker socket
+    worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    worker_socket.connect(('192.168.83.205', 5000))
 
-if __name__ == "__main__":
-    main()
+    # Send tasks to the server and receive results
+    while True:
+        task = queue.get()
+        if task is None:
+            break
+        worker_socket.sendall(pickle.dumps(task))
+        result = pickle.loads(worker_socket.recv(1024))
+        print(result)
+
+    # Clean up the socket
+    worker_socket.close()
+
+# Start the server
+if __name__ == '__main__':
+    start_server()
