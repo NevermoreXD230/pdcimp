5afaf139ed31f28568cafe817b1a1f1ef3b78c08
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 06:39:37 +0500

    Update prime_distributed.erl

--- prime_distributed.erl
+++ prime_distributed.erl
@@ -1,37 +1,48 @@
+% prime_distributed.erl
+
 -module(prime_distributed).
--export([start/1, stop/0, process/2]).
 
-start(Workers) ->
-    spawn(fun() -> loop(Workers) end).
+-export([start/1, start_worker/0, is_prime/1, primes/2]).
 
-stop() ->
-    self() ! stop.
+start_worker() ->
+    register(worker, spawn(fun() -> worker() end)).
 
-process(N, Client) ->
-    self() ! {process, N, Client},
+worker() ->
     receive
-        {result, P} ->
-            Client ! {result, P}
-    end.
+        {From, {is_prime, N}} ->
+            Res = is_prime(N),
+            From ! {self(), {is_prime, Res}}
+    end,
+    worker().
 
-loop(Workers) ->
+start(N) ->
+    start_worker(),
+    {_, Workers} = lists:foldl(
+        fun(_, {Id, Workers}) ->
+            {Id+1, [spawn(worker, worker, []) | Workers]}
+        end,
+        {1, []},
+        lists:seq(1, N)
+    ),
+    lists:foreach(
+        fun(Worker) ->
+            Worker ! {self(), {is_prime, 2}}
+        end,
+        Workers
+    ),
+    primes(Workers, 3).
+
+primes([], _) ->
+    ok;
+primes(Workers, N) ->
     receive
-        {process, N, Client} ->
-            spawn(fun() -> handle(N, Client, Workers) end),
-            loop(Workers);
-        stop ->
-            ok
+        {Worker, {is_prime, Res}} ->
+            case Res of
+                true ->
+                    io:format("~p is prime~n", [N]);
+                false ->
+                    ok
+            end,
+            Worker ! {self(), {is_prime, N+2}},
+            primes(Workers, N+2)
     end.
-
-handle(N, Client, [Worker | Rest]) ->
-    case rpc:call(Worker, fermat, process, [N]) of
-        {ok, P} ->
-            Client ! {result, P},
-            Worker ! {free, self()},
-            loop([Worker | Rest]);
-        _ ->
-            handle(N, Client, Rest)
-    end;
-handle(_, _, []) ->
-    ok.
-
