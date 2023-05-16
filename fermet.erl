f05a54cfc775b751fe11f228413b11dc3e6fce54
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 06:20:00 +0500

    Update fermet.erl

--- fermet.erl
+++ fermet.erl
@@ -1,27 +1,21 @@
--module(fermet).
+-module(fermat).
 -export([is_prime/1]).
 
 is_prime(N) ->
-    is_prime(N, 20).
+    RandomNum = rand:uniform(N-2) + 1,
+    fermat_test(N, RandomNum, 5).
 
-is_prime(_, 0) -> true;
-is_prime(N, K) ->
-    A = rand:uniform(N - 2) + 1,
-    case fermat_test(A, N) of
-        true ->
-            is_prime(N, K - 1);
-        false ->
-            false
+fermat_test(_, _, 0) ->
+    true;
+fermat_test(N, A, TriesLeft) ->
+    case exp_modulo(A, N-1, N) == 1 of
+        true -> fermat_test(N, rand:uniform(N-2) + 1, TriesLeft-1);
+        false -> false
     end.
 
-fermat_test(A, N) ->
-    case expmod(A, N-1, N) of
-        1 -> true;
-        _ -> false
-    end.
-
-expmod(_, 0, N) -> 1;
-expmod(B, E, N) when E rem 2 == 0 ->
-    ((expmod(B, E div 2, N)) rem N) * ((expmod(B, E div 2, N)) rem N) rem N;
-expmod(B, E, N) ->
-    ((B rem N) * (expmod(B, E - 1, N) rem N)) rem N.
+exp_modulo(_, 0, _)-> 1;
+exp_modulo(X, Y, M) when Y rem 2 =:= 0 ->
+    Z = exp_modulo(X, Y div 2, M),
+    (Z * Z) rem M;
+exp_modulo(X, Y, M) ->
+    (X * exp_modulo(X, Y-1, M)) rem M.
