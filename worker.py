56622f51bc28b9e75282821ad80995bbec57b7e2
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 11:57:09 +0500

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,37 +1,18 @@
-import socket
-import pickle
-
-def start_worker():
+# Define a function to start a worker
+def start_worker(queue):
     # Set up the worker socket
     worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    worker_socket.connect(('192.168.83.46', 5001))
+    worker_socket.connect(('192.168.83.205', 5000))
 
-    # Receive the task from the server
-    task = pickle.loads(worker_socket.recv(1024))
-
-    # Generate primes for the task
-    primes = generate_primes(task[0], task[1])
-
-    # Send the primes back to the server
-    worker_socket.sendall(pickle.dumps(primes))
+    # Send tasks to the server and receive results
+    while True:
+        task = queue.get()
+        if task is None:
+            break
+        start, end = task  # Unpack the task tuple
+        worker_socket.sendall(pickle.dumps(task))
+        result = pickle.loads(worker_socket.recv(1024))
+        print(result)
 
     # Clean up the socket
     worker_socket.close()
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
-
-def generate_primes(start, end):
-    primes = []
-    for num in range(start, end+1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-
-if __name__ == '__main__':
-    start_worker()
