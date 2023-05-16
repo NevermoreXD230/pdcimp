29df02f56f23194c6b01dd2356137b744da712f2
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-16 12:24:57 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,76 +1,61 @@
-import socket
-import pickle
+import random
 from multiprocessing import Process, Queue
 
-# Define a function to check if a number is prime
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
+def fermat(n):
+    if n == 1:
+        return True
+
+    for _ in range(10):  # Perform the test multiple times
+        r = random.randint(1, n-1)
+        if pow(r, n-1, n) != 1:
             return False
+
     return True
 
-# Define a function to handle a connection from a worker
-def handle_connection(conn, queue):
-    while True:
-        task = conn.recv(1024)
-        if not task:
-            break
-        start, end = task
-        primes = []
-        for n in range(start, end+1):
-            if is_prime(n):
-                primes.append(n)
-        queue.put(primes)
-    conn.close()
+def server(num_workers):
+    primes_found = []
 
-# Define a function to start the server
-def start_server():
-    # Set up the server socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('192.168.83.205', 5000))
-    server_socket.listen()
+    def worker_task(worker_id, num_queue, result_queue):
+        while True:
+            num = num_queue.get()  # Wait for a number to test
+            if num is None:
+                break
 
-    # Create a queue to store the results from the workers
-    results_queue = Queue()
+            if fermat(num):
+                result_queue.put((worker_id, num))  # Report prime number to server
 
-    # Start two worker processes
+    num_queue = Queue()
+    result_queue = Queue()
+
+    # Start worker processes
     workers = []
-    for i in range(2):
-        p = Process(target=start_worker, args=(results_queue,))
+    for i in range(num_workers):
+        p = Process(target=worker_task, args=(i, num_queue, result_queue))
         p.start()
         workers.append(p)
 
-    # Listen for connections from the workers
+    next_number = 2
     while True:
-        conn, addr = server_socket.accept()
-        p = Process(target=handle_connection, args=(conn, results_queue))
-        p.start()
+        for p in primes_found:
+            if next_number % p == 0:
+                break
+        else:
+            num_queue.put(next_number)  # Send number to a worker for testing
 
-    # Wait for the worker processes to finish
+        while not result_queue.empty():
+            worker_id, prime = result_queue.get()
+            primes_found.append(prime)
+            print(f"Worker {worker_id} found prime: {prime}")
+
+        next_number += 1
+
+    # Stop worker processes
+    for _ in range(num_workers):
+        num_queue.put(None)
+
     for p in workers:
         p.join()
 
-# Define a function to start a worker
-def start_worker(queue):
-    # Set up the worker socket
-    worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    worker_socket.connect(('192.168.83.205', 5000))
-
-    # Send tasks to the server and receive results
-    while True:
-        task = queue.get()
-        if task is None:
-            break
-        worker_socket.sendall(pickle.dumps(task))
-        result = pickle.loads(worker_socket.recv(1024))
-        print(result)
-
-    # Clean up the socket
-    worker_socket.close()
-
-# Start the server
-if __name__ == '__main__':
-    start_server()
+if __name__ == "__main__":
+    num_workers = 2  # Adjust the number of workers as per your requirement
+    server(num_workers)
