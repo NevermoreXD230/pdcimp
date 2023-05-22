df6bc46fded47d7121ac72e4d4c19ad0c6e977d4
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-22 16:00:50 +0500

    Update prime_worker.erl

--- prime_worker.erl
+++ prime_worker.erl
@@ -1,16 +1,22 @@
 -module(prime_worker).
--export([start/0]).
+-export([start/1]).
 
-start() ->
-    spawn(fun() -> init() end).
+start(ServerIP) ->
+    spawn(fun() -> init(ServerIP) end).
 
-init() ->
+init(ServerIP) ->
     io:format("Worker started.~n"),
+    Server = get_server(ServerIP),
     receive
         {prime_to_verify, Prime, Server} ->
             IsPrime = is_prime(Prime),
             Server ! {is_prime, self(), IsPrime}
     end.
+
+get_server(ServerIP) ->
+    {ok, Server} = net_adm:world(),
+    {ok, ServerPID} = net_kernel:connect_node(ServerIP),
+    ServerPID.
 
 is_prime(N) ->
     %% Code to verify if N is prime goes here.
