678e07438f9713e150f8075f27962256f9277c6e
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-22 16:01:15 +0500

    Update prime_server.erl

--- prime_server.erl
+++ prime_server.erl
@@ -6,17 +6,27 @@ start() ->
 
 init() ->
     io:format("Server started.~n"),
+    Worker1 = spawn(fun() -> worker() end),
+    Worker2 = spawn(fun() -> worker() end),
     receive
         {generate_prime, Worker1, Worker2} ->
             Prime = generate_prime(),
             io:format("Generated prime number: ~p~n", [Prime]),
             Worker1 ! {prime_to_verify, Prime, self()},
             Worker2 ! {prime_to_verify, Prime, self()},
-            wait_for_results(Prime)
+            wait_for_results(Prime, 0)
     end.
 
-wait_for_results(Prime) ->
-    wait_for_results(Prime, [], 0).
+worker() ->
+    io:format("Worker started.~n"),
+    receive
+        {prime_to_verify, Prime, Server} ->
+            IsPrime = is_prime(Prime),
+            Server ! {is_prime, self(), IsPrime}
+    end.
+
+wait_for_results(Prime, NumResults) ->
+    wait_for_results(Prime, [], NumResults).
 
 wait_for_results(Prime, Results, NumResults) when NumResults < 2 ->
     receive
@@ -35,3 +45,12 @@ generate_prime() ->
     %% You can use any algorithm of your choice.
     %% For simplicity, we'll return a small prime number in this example.
     17.
+
+is_prime(N) ->
+    %% Code to verify if N is prime goes here.
+    %% You can use any algorithm of your choice.
+    %% For simplicity, we'll return a result based on a small prime number in this example.
+    case N of
+        17 -> "is"
+        _ -> "is not"
+    end.
