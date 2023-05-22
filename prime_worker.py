4aa831a221d5028baa000d02b563fecd66731b08
Qalandar ali <74095947+NevermoreXD230@users.noreply.github.com>
2023-05-22 16:32:51 +0500

    Update prime_worker.py

--- prime_worker.py
+++ prime_worker.py
@@ -1,43 +1,37 @@
 import pika
-import random
-import math
 
-def mpow(N, K, M):
-    if K == 1:
-        return N
-    elif K % 2 == 0:
-        X = mpow(N, K // 2, M)
-        return (X * X) % M
-    else:
-        X = mpow(N, K - 1, M)
-        return (X * N) % M
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.74.46'))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
 
-def fermat(P):
-    if P == 1:
-        return "ok"
+def mpow(n, k, m):
+    if k == 1:
+        return n
+    if k % 2 == 0:
+        x = mpow(n, k // 2, m)
+        return (x * x) % m
     else:
-        for _ in range(10):  # Perform the test 10 times
-            R = random.randint(1, P - 1)
-            T = mpow(R, P - 1, P)
-            if T != 1:
-                return "no"
-        return "ok"
+        x = mpow(n, k - 1, m)
+        return (x * n) % m
+
+def fermat(p):
+    if p == 1:
+        return False
+    for _ in range(5):  # Run the test 5 times for high accuracy
+        r = random.randint(1, p - 1)
+        t = mpow(r, p - 1, p)
+        if t != 1:
+            return False
+    return True
 
 def callback(ch, method, properties, body):
-    prime = int(body)
-    print("Received prime number:", prime)
-    result = fermat(prime)
-    print("Result:", result)
-    ch.basic_publish(exchange='', routing_key=properties.reply_to, body=result)
+    number = int(body)
+    result = fermat(number)
+    response = "ok" if result else "no"
+    ch.basic_publish(exchange='', routing_key=properties.reply_to, body=response)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def start():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='task_queue', durable=True)
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='task_queue', on_message_callback=callback)
-    print("Worker started. Waiting for prime numbers...")
-    channel.start_consuming()
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
-start()
+channel.start_consuming()
